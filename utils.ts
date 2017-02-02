0979b1546b06d33311c4c0abbf5636a1e0043c67
ovrmrw <t.ngc.0403@gmail.com>
2017-02-02 21:19:12 +0900

    Fix

--- utils.ts
+++
@@ -1,7 +0,0 @@
-import { Container, interfaces } from 'inversify';
-
-
-export function rebind<T extends interfaces.Newable<any>>(container: Container, token: T, injectClass: T): void {
-  container.unbind(token)
-  container.bind(token).to(injectClass)
-}
